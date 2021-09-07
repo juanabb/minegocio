c9a0836c6390e01cb9681f3d4faacedbb5f94add
juanbb <agustin291@gmail.com>
2021-09-07 09:07:31 -0300

    Comentario del commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Nav from './Nav.jsx'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <header className="App-header">
+      
+        <p>
+          holis holisss
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,10 @@
+const Nav=() => {
+    return(
+        <nav>   
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        </nav>
+    )
+}
+
+export default Nav;
